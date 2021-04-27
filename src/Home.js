33e75afe872fb3d3fac72efc36d922d370b7e112
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-04-27 12:45:19 +0200

    Added event handling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('Boom Boom', e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
+  }
+
   return ( 
     <div className="home!">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain('mario', e)}>Click Again</button>
     </div>
    );
 }
