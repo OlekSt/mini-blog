8a5c3364ed2e28baae1bd70a34fc1603ba1dcb32
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-04-27 13:47:05 +0200

    Added props

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,16 @@
 import { useState } from 'react';
+import PostList from './PostList';
 
 const Home = () => {
-
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName("fabio");
-    setAge(34);
-  }
-
-
+  const [posts, setPosts] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+  
   return ( 
     <div className="home!">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old.</p>
-      <button onClick={handleClick}>Click Me</button>
+      <PostList posts={posts} title="All posts!"/>
     </div>
    );
 }
