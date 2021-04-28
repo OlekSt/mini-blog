f9cd7961b07f5f1241b9ceceb96ae2dc75e6fb7d
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-04-28 15:50:25 +0200

    Added delete post

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from './useFetch';
 
 const PostDetails = () => {
   const { id } = useParams();
   const { data: post, isPending, error} = useFetch('http://localhost:8000/posts/' + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch('http://localhost:8000/posts/' + post.id, {
+      method: 'DELETE'
+    }).then(()=> {
+      history.push('/')
+    })
+  }
 
   return (
     <div className="post-details">
@@ -14,6 +23,7 @@ const PostDetails = () => {
             <h2>{ post.title }</h2>
             <p>Written by { post.author }</p>
             <div>{ post.body} </div>
+            <button onClick={handleClick}>delete</button>
           </article>
         )}
     </div>
