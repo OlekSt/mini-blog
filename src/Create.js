bc318db2c051324153a5c123faf9dc12cdf59acc
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-04-28 15:12:01 +0200

    Added form for a new post

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
   return ( 
     <div className="create">
       <h2>Add a New Post</h2>
+      <form>
+        <label>Post title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Post body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Post author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="ashley">ashley</option>
+          <option value="luigi">luigi</option>luigi
+        </select>
+        <button>Add Post</button>
+        <p>{ title }</p>
+        <p>{ body }</p>
+        <p>{ author }</p>
+      </form>
     </div>
    );
 }
