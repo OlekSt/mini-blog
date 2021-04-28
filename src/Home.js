088c618bd01305f2817dc6fd5958bd2cd8941f85
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-04-28 11:56:40 +0200

    conditional loading

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import PostList from './PostList';
 
 const Home = () => {
   const [posts, setPosts] = useState(null);
-  
+  const [isPending, setIsPending] = useState(true);
+
   useEffect(async() => {
     fetch('http://localhost:8000/posts')
       .then(res => {
@@ -11,11 +12,13 @@ const Home = () => {
       })
       .then(data => {
         setPosts(data);
+        setIsPending(false);
       })
   }, []);
 
   return ( 
     <div className="home!">
+      { isPending && <div>LOADING....</div> }
       {posts && <PostList posts={posts} title="All posts!" />}
     </div>
    );
